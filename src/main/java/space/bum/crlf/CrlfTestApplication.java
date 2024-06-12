7372ef70a68752d6f57ca304b7cf6f422d239ac6
osparking <jbpark03@gmail.com>
2024-06-12 18:16:43 +0900

    test start

---
+++ src/main/java/space/bum/crlf/CrlfTestApplication.java
@@ -0,0 +1,13 @@
+package space.bum.crlf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrlfTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrlfTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bum/crlf/CrlfTestApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum.crlf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrlfTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
